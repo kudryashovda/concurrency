5a05043947e33e0e1e1f40e8a676f88e3ee6a176
Kudryashov Dmitrii <kudryashovda@simicon.com>
2023-01-22 19:01:32 +0300

    Add an async parallel summ

--- vector_sum/main.cpp
+++ vector_sum/main.cpp
@@ -1,5 +1,6 @@
 #include "log_duration.h"
 #include <cassert>
+#include <future>
 #include <numeric>
 #include <vector>
 
@@ -7,11 +8,37 @@
 
 using namespace std;
 
-size_t calVectorSumSeq(const vector<size_t> &vec) {
+size_t calVectorPartialSum(const vector<size_t> &vec, size_t beg_idx,
+                           size_t end_idx) {
   size_t summ = 0;
-  for (const auto &item : vec) {
-    summ += item;
+  for (size_t idx = beg_idx; idx < end_idx; ++idx) {
+    summ += vec[idx];
   }
+  return summ;
+}
+
+size_t calVectorSumSeq(const vector<size_t> &vec) {
+  return calVectorPartialSum(vec, 0, vec.size());
+}
+
+size_t calVectorSumParAsync(const vector<size_t> &vec) {
+  size_t idx[5];
+  idx[0] = 0;
+  idx[1] = vec.size() / 4;
+  idx[2] = vec.size() / 4 * 2;
+  idx[3] = vec.size() / 4 * 3;
+  idx[4] = vec.size();
+
+  vector<future<size_t>> futures;
+  for (int i = 0; i < 4; ++i) {
+    futures.push_back(async(calVectorPartialSum, ref(vec), idx[i], idx[i + 1]));
+  }
+
+  size_t summ = 0;
+  for (int i = 0; i < 4; ++i) {
+    summ += futures[i].get();
+  }
+
   return summ;
 }
 
@@ -28,4 +55,9 @@ int main() {
     assert(summ == expected_summ);
   }
 
+  {
+    LOG_DURATION("calVectorSumParAsync");
+    const size_t summ = calVectorSumParAsync(numbers);
+    assert(summ == expected_summ);
+  }
 }
